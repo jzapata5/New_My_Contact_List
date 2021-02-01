901d37efe912b6b4406bd947778c8f2d0606e07c
Jose Cabral <jose12marte@yahoo.com>
2021-01-31 20:12:20 -0500

    Updating the project, added various features

--- app/src/main/java/com/example/mycontactlist/MainActivity.java
+++ app/src/main/java/com/example/mycontactlist/MainActivity.java
@@ -1,14 +1,132 @@
 package com.example.mycontactlist;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
+import com.example.mycontactlist.DatePickerDialog.SaveDateListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.view.View.*;
 
-public class MainActivity extends AppCompatActivity {
+import android.text.format.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.text.format.DateFormat.format;
+import static java.text.DateFormat.*;
+
+public class MainActivity extends AppCompatActivity implements SaveDateListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+        initToggleButton();
+        initChangeDateButton();
     }
+
+    private void initListButton() {
+        ImageButton contactList = findViewById(R.id.imageButtonList); // Variable to hold the ImageButton
+        contactList.setOnClickListener(new View.OnClickListener() { // Listener is added to the ImageButton to make it respond to different things
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactListActivity.class); // A mew Intent is created, the Intent's constructors requires reference to the current activity and know what activity to start
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // An Intent flag is set to alert the operating system to not make multiple copes of the same activity
+                startActivity(intent); // And listen
+            }
+        });
+    }
+
+    private void initMapButton() {
+        ImageButton contactList = findViewById(R.id.imageButtonMap); // Same as above
+        contactList.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent); // And listen
+                setForEditing(false);
+            }
+        });
+    }
+
+    private void initSettingsButton() {
+        ImageButton contactList = findViewById(R.id.imageButtonSettings); // Same as above
+        contactList.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent); // And listen
+            }
+        });
+    }
+
+    private void initToggleButton() {
+        final ToggleButton editToggle = findViewById(R.id.toggleButtonEdit);
+        editToggle.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
+    }
+
+    private void setForEditing(boolean enabled) {
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipCode = findViewById(R.id.editZipCode);
+        EditText editHome = findViewById(R.id.editHome);
+        EditText editCell= findViewById(R.id.editCity);
+        EditText editEmail = findViewById(R.id.editEmail);
+        Button buttonChange = findViewById(R.id.btnBirthday);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipCode.setEnabled(enabled);
+        editHome.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+
+        if (enabled) {
+            editName.requestFocus();
+        }
+    }
+
+    private void initChangeDateButton() {
+        Button changeDate = findViewById(R.id.btnBirthday);
+        changeDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                DatePickerDialog datePickerDialog = new DatePickerDialog(); // We create the dialog here
+                datePickerDialog.show(fm, "DatePick"); // DialogFragment.show() method shows the actual dialog. For this to happen, we need the fragment manager that is above
+            }
+        });
+    }
+
+    @Override
+    public void didFinishDatePickerDialog(Calendar selectedTime) {
+        TextView birthDay = findViewById(R.id.textBirthday);
+        Log.i("Hello there", "hello");
+        birthDay.setText(DateFormat.format("MM/dd/yyyy", selectedTime));
+
+    }
+
+
 }
